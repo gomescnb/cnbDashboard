5c713597f718ab9ac1fdd0a1abacc6caaedaf9d0
gomescnb <C0012240@cenibra.com.br>
2024-07-02 17:03:07 -0300

    updates to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Dashboard is ready!');
-});
+const symbolInfoSection = document.getElementById('symbol-info');
+const widgetScript = document.createElement('script');
+widgetScript.type = 'text/javascript';
+widgetScript.src = 'https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js';
+widgetScript.async = true;
+widgetScript.innerHTML = `
+{
+	"symbolsGroups": [
+	{
+		"name": "Forex",
+		"originalName": "Forex",
+		"symbols": [
+		{
+			"name": "USDBRL",
+			"displayName": "USD to BRL"
+		},
+		{
+			"name": "FX:GBPUSD",
+			"displayName": "GBP to USD"
+		},
+		{
+			"name": "FX:USDJPY",
+			"displayName": "USD to JPY"
+		},
+		{
+			"name": "FX:USDCHF",
+			"displayName": "USD to CHF"
+		},
+		{
+			"name": "FX:AUDUSD",
+			"displayName": "AUD to USD"
+		},
+		{
+			"name": "FX:USDCAD",
+			"displayName": "USD to CAD"
+		}
+		],
+		"symbols2": [
+		{
+			"name": "FX:EURUSD",
+			"displayName": "EUR to USD"
+		}
+		]
+	}
+	],
+	"showSymbolLogo": true,
+	"isTransparent": false,
+	"colorTheme": "dark",
+	"locale": "br",
+	"backgroundColor": "#131722"
+}
+`;
+symbolInfoSection.appendChild(widgetScript);
